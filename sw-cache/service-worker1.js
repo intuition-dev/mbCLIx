29623a249fcd6b8a05a7d3ac615320f0c8e75a97
Vic <vic@eml.cc>
2019-01-15 22:34:14 -0500

    sw

--- sw-cache/service-worker1.js
+++ sw-cache/service-worker1.js
@@ -1,8 +1,42 @@
 
 
-// https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+// http://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+// http://deanhume.com/create-a-really-really-simple-offline-page-using-service-workers
 
-self.addEventListener('fetch', function fetcher (event) {
-   console.info('sw', event.request.url)
-   event.respondWith(fetch(event.request))
+this.addEventListener('fetch', event => {
+   if (event.request.mode === 'navigate' ) {
+         event.respondWith(
+           fetch(event.request.url).catch(error => {
+               // Return the offline page
+               return caches.match(offlineUrl)
+           })
+     )
+   }
+   else {
+      // Respond with everything else if we can
+      event.respondWith(caches.match(event.request)
+            .then(function (response) {
+               return response || fetch(event.request)
+            })
+         )
+      }
  })
+
+
+//put in cache
+var cacheVersion = 1
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+}
+const offlineUrl = 'index.html'
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          'logo.jpg',
+          offlineUrl
+      ])
+    })
+  )
+})
