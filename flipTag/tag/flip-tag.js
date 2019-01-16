1485240b914bdd514c073ea348e3adabc154c74e
Vic <vic@eml.cc>
2019-01-16 14:13:13 -0500

    cleaner

--- flipTag/tag/flip-tag.js
+++ flipTag/tag/flip-tag.js
@@ -1,67 +1,67 @@
 
-riot.tag2('liz-flipcard', '<div class="cardwrap {flipx:flipx,flipy:flipy,backflip:backflip,back45:back45}"><yield></yield></div>', '', '', function(opts) {
-    this.flipx = false;
-    this.flipy = false;
+riot.tag2('liz-flipcard', '<div class="cardwrap {flipx:flipx, flipy:flipy, backflip:backflip, back45:back45}"><yield></yield></div>', '', '', function(opts) {
+    this.flipx = false
+    this.flipy = false
 
     thiz = this
 
     this.on('mount', function() {
-      var _ref;
+      var _ref
       if (!this.root.style.width) {
-        this.root.style.width = "100px";
+        this.root.style.width = '100px'
       }
       if (!this.root.style.height) {
-        this.root.style.height = "100px";
+        this.root.style.height = '100px'
       }
-      this.root.style.display = "block";
+      this.root.style.display = 'block'
       if ((_ref = opts.direction) != null ? _ref.match(/[xy]{2}/) : void 0) {
-        thiz.back45 = true;
+        thiz.back45 = true
       }
-      if (opts.direction === "y") {
-        thiz.backflip = true;
+      if (opts.direction === 'y') {
+        thiz.backflip = true
       }
 
-      [].slice.call(this.root.querySelectorAll('.front,.back,.cardwrap')).forEach((function(thiz) {
+      [].slice.call(this.root.querySelectorAll('.front, .back, .cardwrap')).forEach((function(thiz) {
         return function(elem) {
-          elem.style.width = thiz.root.offsetWidth + "px";
-          elem.style.height = thiz.root.offsetHeight + "px";
-        };
-      })(this));
+          elem.style.width = thiz.root.offsetWidth + 'px'
+          elem.style.height = thiz.root.offsetHeight + 'px'
+        }
+      })(this))
 
-      this.update();
-    });
+      this.update()
+    })
 
     this.toggle = function() {
-        var _ref, _ref1;
-        if (opts.direction !== "y" || ((_ref = opts.direction) != null ? _ref.match(/x/) : void 0)) {
-          this.flipx = !this.flipx;
+        var _ref, _ref1
+        if (opts.direction !== 'y' || ((_ref = opts.direction) != null ? _ref.match(/x/) : void 0)) {
+          this.flipx = !this.flipx
         }
         if ((_ref1 = opts.direction) != null ? _ref1.match(/y/) : void 0) {
-          this.flipy = !this.flipy;
+          this.flipy = !this.flipy
         }
-         this.update();
+         this.update()
     }.bind(this)
 
     this.front = function () {
-        var _ref, _ref1;
-        if (opts.direction !== "y" || ((_ref = opts.direction) != null ? _ref.match(/x/) : void 0)) {
-          this.flipx = false;
+        var _ref, _ref1
+        if (opts.direction !== 'y' || ((_ref = opts.direction) != null ? _ref.match(/x/) : void 0)) {
+          this.flipx = false
         }
         if ((_ref1 = opts.direction) != null ? _ref1.match(/y/) : void 0) {
-          this.flipy = false;
+          this.flipy = false
         }
-         this.update();
+         this.update()
     }.bind(this)
 
     this.back = function() {
-        var _ref, _ref1;
-        if (opts.direction !== "y" || ((_ref = opts.direction) != null ? _ref.match(/x/) : void 0)) {
-          this.flipx = true;
+        var _ref, _ref1
+        if (opts.direction !== 'y' || ((_ref = opts.direction) != null ? _ref.match(/x/) : void 0)) {
+          this.flipx = true
         }
         if ((_ref1 = opts.direction) != null ? _ref1.match(/y/) : void 0) {
-          this.flipy = true;
+          this.flipy = true
         }
-         this.update();
+         this.update()
 
     }.bind(this)
 });
