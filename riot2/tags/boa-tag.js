f24d1a0d703aa8be60f1e7df8dafdc94ea2df840
Liza K <liza.kislyakova@gmail.com>
2019-01-24 14:36:13 +0300

    tags attributes functionallity add

--- riot2/tags/boa-tag.js
+++ riot2/tags/boa-tag.js
@@ -1,23 +1,22 @@
 
-riot.tag2('boa-tag', '<p class="num">{num}</p> <form> <input placeholder="Enter any number" type="number"> <input placeholder="Enter any number" type="number"> </form><yield></yield>', '', '', function(opts) {
+riot.tag2('boa-tag', '<form><yield></yield> <div class="h-row"> <button type="submit" onclick="{init}">{opts.text}</button> <p class="num">{num}</p> </div> </form>', '', '', function(opts) {
+    let type = opts.type;
 
-    this.getSum = function(e) {
+    this.init = function(e) {
         e.preventDefault();
-        let arg1 = $('input:first-of-type', this.root).val();
-        let arg2 = $('input:last-of-type', this.root).val();
-        this.doSomething(arg1, arg2);
-    }.bind(this)
-    this.doSomething = function(arg1, arg2) {
-        this.update({num: +arg1+ +arg2});
-    }.bind(this)
 
-    this.getSub = function(e) {
-        e.preventDefault();
-        let arg1 = $('input:first-of-type', this.root).val();
-        let arg2 = $('input:last-of-type', this.root).val();
-        this.doSomething2(arg1, arg2);
-    }.bind(this)
-    this.doSomething2 = function(arg1, arg2) {
-        this.update({num: +arg1 - +arg2});
+        if (type === 'sum') {
+
+            let arg1 = $('input:first-of-type', this.root).val();
+            let arg2 = $('input:last-of-type', this.root).val();
+
+            this.update({num: +arg1+ +arg2});
+        } else {
+
+            let arg1 = $('input:first-of-type', this.root).val();
+            let arg2 = $('input:last-of-type', this.root).val();
+
+            this.update({num: +arg1 - +arg2});
+        }
     }.bind(this)
 });
