0356a7006a3b138d6bda8d6e8e15a5717c83d904
Liza K <liza.kislyakova@gmail.com>
2019-01-24 20:26:13 +0300

    tag fix

--- riot2/tags/boa-tag.js
+++ riot2/tags/boa-tag.js
@@ -1,22 +1,23 @@
 
-riot.tag2('boa-tag', '<form><yield></yield> <div class="h-row"> <button type="submit" onclick="{init}">{opts.text}</button> <p class="num">{num}</p> </div> </form>', '', '', function(opts) {
-    let type = opts.type;
+riot.tag2('boa-tag', '<form><yield></yield> <div class="h-row"> <button type="submit" onclick="{this.handleClick}">{opts.text}</button> <p class="num">{num}</p> </div> </form>', '', '', function(opts) {
+    this.on('before-mount', function() {
+        this.handleClick = function(e) {
+            e.preventDefault();
 
-    this.init = function(e) {
-        e.preventDefault();
+            let type = opts.type;
+            if (type === 'sum') {
 
-        if (type === 'sum') {
+                let arg1 = $('input:first-of-type', this.root).val();
+                let arg2 = $('input:last-of-type', this.root).val();
 
-            let arg1 = $('input:first-of-type', this.root).val();
-            let arg2 = $('input:last-of-type', this.root).val();
+                this.update({num: +arg1+ +arg2});
+            } else {
 
-            this.update({num: +arg1+ +arg2});
-        } else {
+                let arg1 = $('input:first-of-type', this.root).val();
+                let arg2 = $('input:last-of-type', this.root).val();
 
-            let arg1 = $('input:first-of-type', this.root).val();
-            let arg2 = $('input:last-of-type', this.root).val();
-
-            this.update({num: +arg1 - +arg2});
-        }
-    }.bind(this)
+                this.update({num: +arg1 - +arg2});
+            }
+        }.bind(this)
+    });
 });
