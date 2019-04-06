050cfb5cfb80cfaca01b343e2afb4ca852f84769
Vic <uptimevic@gmail.com>
2019-04-06 18:55:01 -0400

    sync

---
+++ future/servSrv/servCli/3rd/jayson/method.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const utils = require('./utils');
+const _ = require('lodash');
+
+/**
+ * @summary Constructor for a Jayson Method
+ * @class Method
+ * @param {Function} [handler] Function to set as handler
+ * @param {Object} [options] 
+ * @param {Function} [options.handler] Same as separate handler
+ * @param {Boolean} [options.useContext=false] When true, the handler expects a context object
+ * @param {Array|Object} [options.params] Defines params that the handler accepts
+ */
+const Method = function(handler, options) {
+
+  if(!(this instanceof Method)) {
+    return new Method(handler, options);
+  }
+
+  // only got passed options
+  if(_.isPlainObject(handler)) {
+    options = handler;
+    handler = null;
+  }
+
+  const defaults = {
+    useContext: false,
+  };
+
+  options = options || {};
+
+  this.options = utils.merge(defaults, options);
+  this.handler = handler || options.handler;
+};
+
+module.exports = Method;
+
+/**
+ * @summary Returns the handler function associated with this method
+ * @return {Function}
+ */
+Method.prototype.getHandler = function() {
+  return this.handler;
+};
+
+/**
+ * @summary Sets the handler function associated with this method
+ * @param {Function} handler
+ */
+Method.prototype.setHandler = function(handler) {
+  this.handler = handler;
+};
+
+/**
+ * @summary Prepare parameters for the method handler
+ * @private
+ */
+Method.prototype._getHandlerParams = function(params) {
+  const options = this.options;
+
+  const isObjectParams = !_.isArray(params) && _.isObject(params) && params;
+  const isArrayParams = _.isArray(params);
+
+  switch(true) {
+
+      // handler always gets an array
+    case options.params === Array:
+      return isArrayParams ? params : _.toArray(params);
+
+      // handler always gets an object
+    case options.params === Object:
+      return isObjectParams ? params : _.toPlainObject(params);
+
+      // handler gets a list of defined properties that should always be set
+    case _.isArray(options.params): {
+      const undefinedParams = _.reduce(options.params, function(undefinedParams, key) {
+        undefinedParams[key] = undefined;
+        return undefinedParams;
+      }, {});
+      return _.extend(undefinedParams, _.pick(params, _.keys(params)));
+    }
+
+      // handler gets a map of defined properties and their default values
+    case _.isPlainObject(options.params):
+      return _.extend({}, options.params, _.pick(params, _.keys(params)));
+
+      // give params as is
+    default:
+      return params;
+
+  }
+
+};
+
+/**
+ * @summary Executes this method in the context of a server
+ * @param {Server} server
+ * @param {Array|Object} requestParams
+ * @param {Object} [context]
+ * @param {Function} callback
+ */
+Method.prototype.execute = function(server, requestParams, context, callback) {
+  if(typeof(context) === 'function') {
+    callback = context;
+    context = {};
+  }
+
+  if(!context) {
+    context = {};
+  }
+
+  // when useContext is true, the handler gets a context object every time
+  const useContext = Boolean(this.options.useContext);
+  const handler = this.getHandler();
+  const params = this._getHandlerParams(requestParams);
+
+  const args = useContext ? [params, context, callback] : [params, callback];
+  return handler.call(server, ...args);
+};
---
+++ future/servSrv/servCli/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const jaysonBrowserClient = require('3rd/jayson/client/browser');
+const fetch = require('node-fetch');
+
+const callServer = function(request, callback) {
+  const options = {
+    method: 'POST',
+    body: request,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+
+  fetch('http://localhost:3000', options)
+    .then(function(res) { return res.text(); })
+    .then(function(text) { callback(null, text); })
+    .catch(function(err) { callback(err); });
+};
+
+const client = jaysonBrowserClient(callServer, {
+  // other options go here
+});
+
+client.request('multiply', [5, 5], function(err, error, result) {
+  if(err) throw err;
+  console.log(result); // 25
+});
