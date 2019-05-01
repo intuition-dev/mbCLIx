0075531212087713ae1bfdd3d828ad1c8e2ffc68
Liza K <liza.kislyakova@gmail.com>
2019-05-01 19:22:51 +0300

    [chrome-ext]: check for admin in yaml and redirect on click add

--- webAdminSupport/ext/app.js
+++ webAdminSupport/ext/app.js
@@ -1,49 +1,61 @@
-var bglog = function(obj) {
-	if(chrome && chrome.runtime) {
-		chrome.runtime.sendMessage({type: "bglog", obj: obj});
-	}
+let bgPage = chrome.extension.getBackgroundPage();
+
+function consoleLog(msg) {
+   bgPage.console.info.apply(null, arguments);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+   chrome.runtime.onMessage.addListener(
+      function (message, callback) {
+         consoleLog("MSG:", message);
+      }
+   );
 
-   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-         tabs.forEach(element => {       
-            var url = new URL(element.url);
-            var domain = url.hostname;
-            var map = 'http://' + domain + '/map.yaml';
-            chrome.extension.getBackgroundPage().console.log('map ------>', map);
+   consoleLog('isActive', bgPage.isActive());
+   if (bgPage.isActive()) {
+      consoleLog($('#check'), 'ON');
 
-            // $.get(map, function(data) {
-            //    alert(data);
-            //    chrome.extension.getBackgroundPage().console.log('data ------>', data);
-            // },function(data) {
-            //    alert(data);
-            //    chrome.extension.getBackgroundPage().console.log('error ------>', data);
-            // });
-
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", chrome.extension.getURL('/map.yaml'), true);
-            xhr.onreadystatechange = function() {
-               chrome.extension.getBackgroundPage().console.log('xhr ------>', xhr);
-               alert('onreadeStateChange', xhr.responseText);
-            }
-            xhr.onload = function() {
-               chrome.extension.getBackgroundPage().console.log('xhr load ------>', xhr);
-               alert('load', xhr.responseText);
-            }
-            xhr.onerror = function(error) {
-               alert('error', error);
-            }
-            xhr.send();
-
-            var checkPageButton = document.getElementById('check');
-            checkPageButton.addEventListener('click', function() {
-               // TODO redirect to admin CMS
-            });
-
-         });
+      var checkPageButton = document.getElementById('check');
+      checkPageButton.disabled = false;
+      checkPageButton.addEventListener('click', function() {
+         // redirect to WebAdmin
+         chrome.tabs.update({url: "http://157.230.189.157:9080"});
       });
-   });
+   } else {
+      consoleLog($('#check'), 'OFF');
+   }
+
+
+   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+   //    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+   //       tabs.forEach(element => {       
+   //          var url = new URL(element.url);
+   //          var domain = url.hostname;
+   //          var map = 'http://' + domain + '/map.yaml';
+   //          // consoleLog('map ------>', map);
+
+   //          $.get(map)
+   //             .done(function(res){
+   //                let map = jsyaml.load(res);
+   //                consoleLog('RES:', res, map.isAdmin);
+   //                if (typeof map.isAdmin !== 'undefined' && map.isAdmin === true) {
+   //                   chrome.browserAction.setBadgeText({text:"ready"});
+   //                } else {
+   //                   chrome.browserAction.setBadgeText({text:"X"});
+   //                }
+   //             })
+   //             .fail(function(err){
+   //                consoleLog('ERR:', err);
+   //                chrome.browserAction.setBadgeText({text:"X"});
+   //             });
+
+   //          var checkPageButton = document.getElementById('check');
+   //          checkPageButton.addEventListener('click', function() {
+   //             // TODO redirect to admin CMS
+   //          });
+
+   //       });
+   //    });
+   // });
 
 }, false);
---
+++ webAdminSupport/ext/background.js
@@ -0,0 +1,52 @@
+let active = false;
+
+let activate = () => {
+    chrome.browserAction.setBadgeText({ text: "ready" });
+    active = true;
+    console.info("Extension is active");
+}
+
+let deactivate = () => {
+    chrome.browserAction.setBadgeText({ text: "N/A" });
+    active = false;
+    console.info("Extension deactivated");
+}
+
+function isActive() { return active; }
+
+chrome.tabs.onActivated.addListener(function (info) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
+            tabs.forEach(function (activeTab) {
+                if (typeof activeTab.url !== 'undefined') {
+                    console.info('activeTab ------>', activeTab, activeTab.url);
+                    chrome.extension.sendMessage('hello');
+
+                    var url = new URL(activeTab.url);
+                    var domain = url.hostname;
+                    if (url.protocol === 'http:' || url.protocol === 'https:') {
+                        var map = url.protocol + '//' + domain + '/map.yaml';
+                        console.info('map ------>', map);
+
+                        $.get(map)
+                            .done(function (res) {
+                                let map = jsyaml.load(res);
+                                console.info('RES:', res, map.isAdmin);
+                                if (typeof map.isAdmin !== 'undefined' && map.isAdmin === true) {
+                                    activate();
+                                } else {
+                                    deactivate();
+                                }
+                            })
+                            .fail(deactivate);
+                    } else {
+                        deactivate();
+                    }
+                } else {
+                    deactivate();
+                }
+
+            })
+        })
+    })
+});
