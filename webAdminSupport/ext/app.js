6c86b7091e753eda3d3e20839e15e6f7e8e45aa1
Liza K <liza.kislyakova@gmail.com>
2019-05-01 20:54:27 +0300

    [chrome-ext]: redirect to WebAdmin add, icon update

--- webAdminSupport/ext/app.js
+++ webAdminSupport/ext/app.js
@@ -1,61 +1,61 @@
 let bgPage = chrome.extension.getBackgroundPage();
 
 function consoleLog(msg) {
-   bgPage.console.info.apply(null, arguments);
+    bgPage.console.info.apply(null, arguments);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-   chrome.runtime.onMessage.addListener(
-      function (message, callback) {
-         consoleLog("MSG:", message);
-      }
-   );
+    chrome.runtime.onMessage.addListener(
+        function (message, callback) {
+            consoleLog("MSG:", message);
+        }
+    );
 
-   consoleLog('isActive', bgPage.isActive());
-   if (bgPage.isActive()) {
-      consoleLog($('#check'), 'ON');
+    consoleLog('isActive', bgPage.isActive());
+    if (bgPage.isActive()) {
+        consoleLog($('#check'), 'ON');
 
-      var checkPageButton = document.getElementById('check');
-      checkPageButton.disabled = false;
-      checkPageButton.addEventListener('click', function() {
-         // redirect to WebAdmin
-         chrome.tabs.update({url: "http://157.230.189.157:9080"});
-      });
-   } else {
-      consoleLog($('#check'), 'OFF');
-   }
+        var checkPageButton = document.getElementById('check');
+        checkPageButton.disabled = false;
+        checkPageButton.addEventListener('click', function() {
+            // redirect to WebAdmin
+            chrome.tabs.update({url: "http://157.230.189.157:9080"});
+        });
+    } else {
+        consoleLog($('#check'), 'OFF');
+    }
 
 
-   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-   //    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-   //       tabs.forEach(element => {       
-   //          var url = new URL(element.url);
-   //          var domain = url.hostname;
-   //          var map = 'http://' + domain + '/map.yaml';
-   //          // consoleLog('map ------>', map);
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    //       tabs.forEach(element => {       
+    //          var url = new URL(element.url);
+    //          var domain = url.hostname;
+    //          var map = 'http://' + domain + '/map.yaml';
+    //          // consoleLog('map ------>', map);
 
-   //          $.get(map)
-   //             .done(function(res){
-   //                let map = jsyaml.load(res);
-   //                consoleLog('RES:', res, map.isAdmin);
-   //                if (typeof map.isAdmin !== 'undefined' && map.isAdmin === true) {
-   //                   chrome.browserAction.setBadgeText({text:"ready"});
-   //                } else {
-   //                   chrome.browserAction.setBadgeText({text:"X"});
-   //                }
-   //             })
-   //             .fail(function(err){
-   //                consoleLog('ERR:', err);
-   //                chrome.browserAction.setBadgeText({text:"X"});
-   //             });
+    //          $.get(map)
+    //             .done(function(res){
+    //                let map = jsyaml.load(res);
+    //                consoleLog('RES:', res, map.isAdmin);
+    //                if (typeof map.isAdmin !== 'undefined' && map.isAdmin === true) {
+    //                   chrome.browserAction.setBadgeText({text:"ready"});
+    //                } else {
+    //                   chrome.browserAction.setBadgeText({text:"X"});
+    //                }
+    //             })
+    //             .fail(function(err){
+    //                consoleLog('ERR:', err);
+    //                chrome.browserAction.setBadgeText({text:"X"});
+    //             });
 
-   //          var checkPageButton = document.getElementById('check');
-   //          checkPageButton.addEventListener('click', function() {
-   //             // TODO redirect to admin CMS
-   //          });
+    //          var checkPageButton = document.getElementById('check');
+    //          checkPageButton.addEventListener('click', function() {
+    //             // TODO redirect to admin CMS
+    //          });
 
-   //       });
-   //    });
-   // });
+    //       });
+    //    });
+    // });
 
 }, false);
--- webAdminSupport/ext/background.js
+++ webAdminSupport/ext/background.js
@@ -1,52 +1,59 @@
 let active = false;
 
 let activate = () => {
-    chrome.browserAction.setBadgeText({ text: "ready" });
+    // chrome.browserAction.setBadgeText({ text: "ready" });
     active = true;
     console.info("Extension is active");
+    chrome.browserAction.setIcon({path:"icon.png"});
+
 }
 
 let deactivate = () => {
-    chrome.browserAction.setBadgeText({ text: "N/A" });
+    // chrome.browserAction.setBadgeText({ text: "N/A" });
     active = false;
     console.info("Extension deactivated");
+    chrome.browserAction.setIcon({path:"icon_disabled.png"});
 }
 
 function isActive() { return active; }
 
-chrome.tabs.onActivated.addListener(function (info) {
+let checkExtStatus = function () {
+    deactivate();
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
             tabs.forEach(function (activeTab) {
                 if (typeof activeTab.url !== 'undefined') {
                     console.info('activeTab ------>', activeTab, activeTab.url);
                     chrome.extension.sendMessage('hello');
-
+                    
                     var url = new URL(activeTab.url);
                     var domain = url.hostname;
                     if (url.protocol === 'http:' || url.protocol === 'https:') {
                         var map = url.protocol + '//' + domain + '/map.yaml';
                         console.info('map ------>', map);
-
+                        
                         $.get(map)
-                            .done(function (res) {
-                                let map = jsyaml.load(res);
-                                console.info('RES:', res, map.isAdmin);
-                                if (typeof map.isAdmin !== 'undefined' && map.isAdmin === true) {
-                                    activate();
-                                } else {
-                                    deactivate();
-                                }
-                            })
-                            .fail(deactivate);
+                        .done(function (res) {
+                            let map = jsyaml.load(res);
+                            console.info('RES:', res, map.isAdmin);
+                            if (typeof map.isAdmin !== 'undefined' && map.isAdmin === true) {
+                                activate();
+                            } else {
+                                deactivate();
+                            }
+                        })
+                        .fail(deactivate);
                     } else {
                         deactivate();
                     }
                 } else {
                     deactivate();
                 }
-
+                
             })
         })
     })
-});
+};
+
+chrome.tabs.onUpdated.addListener(checkExtStatus);
+chrome.tabs.onActivated.addListener(checkExtStatus);
