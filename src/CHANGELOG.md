001de5032b6b1cdd44b92a2effa205dc3bbb0d24
Vic <vic@eml.cc>
2020-04-03 12:18:00 -0400

    7

--- src/CHANGELOG.md
+++ src/CHANGELOG.md
@@ -1,4 +1,4 @@
-## 8.3.5
+## 8.4.7
 - renamed sqlite name to BaseNDBSi from BaseDBL
 - added new terseB log
 
--- src/lib/mbakeX.ts
+++ src/lib/mbakeX.ts
@@ -3,7 +3,7 @@
 
 export class MBakeX {
    static verx() {
-      return 'v8.4.5'
+      return 'v8.4.7'
    }
    static date(): string {
       return new Date().toISOString()
